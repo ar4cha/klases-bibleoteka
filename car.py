774cf7b7a8254571b57e4f86dc1985aa5d0a7c88
ar4cha <masta2288@gmail.com>
2021-09-08 10:55:00 +0300

    first commit

---
+++ car.py
@@ -0,0 +1,12 @@
+class Car:
+
+    def __init__(self, name, years , cena):
+        self.name = name
+        self.years = years
+        self.cena = cena
+
+    def firstcarkilometry(self):
+        return"hi in this car have 100000 kilometry"
+
+    def secondcarkilometry(self):
+        return "Hello in this car have 50000 kilometry"
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import car as masina
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+firstCar= masina.Car("Mercedes" , 2013, 224)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+secondCar = masina.Car("Bnw" , 2014 ,3000 )
+
+print(secondCar.name , secondCar.years)
+print(firstCar.name , firstCar.cena)
+
+print(firstCar.firstcarkilometry())
+print(secondCar.secondcarkilometry())
